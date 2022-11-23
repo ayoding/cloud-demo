f77778bd8400e42ae8db7b52f320ab6b505401ce
ouy <532027450@qq.com>
2022-11-23 16:23:56 +0800

    master commit 1

---
+++ order-service/src/main/java/cn/itcast/order/Config/OrderConfig.java
@@ -0,0 +1,37 @@
+package cn.itcast.order.Config;
+
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class OrderConfig {
+    /**
+     * 发送http请求
+     *
+     * @return {@link RestTemplate}
+     */
+    @LoadBalanced //负载均衡
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    /**
+     * 随机规则
+     *  全局有效
+     * @return {@link IRule}
+     */
+    @Bean
+    public IRule randomRule(){
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(2);
+        return new RandomRule();
+    }
+}
---
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -0,0 +1,34 @@
+package cn.itcast.order.service;
+
+import cn.itcast.order.mapper.OrderMapper;
+import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+
+        String url="http://userservice/user/"+order.getUserId();
+
+
+
+        //通过restTemplate发送Get请求得到User对象信息
+        User user = restTemplate.getForObject(url, User.class);
+        //保存user到order
+        order.setUser(user);
+        // 4.返回
+        return order;
+    }
+}
