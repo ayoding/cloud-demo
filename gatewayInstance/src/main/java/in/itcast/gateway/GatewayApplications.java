691d4477bdb2e6b51b1757bbe94d80a0de18c85c
ouy <532027450@qq.com>
2022-12-01 19:24:18 +0800

    gateway网关配置完成

---
+++ gatewayInstance/src/main/java/in/itcast/gateway/GatewayApplications.java
@@ -0,0 +1,11 @@
+package in.itcast.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplications {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplications.class,args);
+    }
+}
